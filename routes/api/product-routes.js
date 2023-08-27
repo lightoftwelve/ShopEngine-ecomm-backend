a235e31daf0cc25f5ad58a293dd68c4119a3773b
lightoftwelve <info@lightoftwelve.com>
2023-08-26 23:18:04 -0400

    completed routes on product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -35,6 +35,75 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// create new product
+router.post('/', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+
+    if (req.body.tagIds && req.body.tagIds.length) {
+      const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: product.id,
+          tag_id,
+        };
+      });
+      await ProductTag.bulkCreate(productTagIdArr);
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// update product
+router.put('/:id', async (req, res) => {
+  try {
+    const product = await Product.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    const productTags = await ProductTag.findAll({ where: { product_id: req.params.id } });
+
+    const productTagIds = productTags.map(({ tag_id }) => tag_id);
+    const newProductTags = req.body.tagIds.filter((tag_id) => !productTagIds.includes(tag_id)).map((tag_id) => {
+      return {
+        product_id: req.params.id,
+        tag_id,
+      };
+    });
+
+    const productTagsToRemove = productTags.filter(({ tag_id }) => !req.body.tagIds.includes(tag_id)).map(({ id }) => id);
+
+    await ProductTag.destroy({ where: { id: productTagsToRemove } });
+    await ProductTag.bulkCreate(newProductTags);
+
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedProduct = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deletedProduct) {
+      return next(notFound('No product found with this id!'));
+    }
+    res.json(deletedProduct);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
+
 // Starter Code:
 
 // const router = require('express').Router();
