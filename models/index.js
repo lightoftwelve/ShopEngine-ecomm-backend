fb338466f25dc2ef01067eb4ac376db7f6492b07
lightoftwelve <info@lightoftwelve.com>
2023-08-26 21:38:10 -0400

    added product.init to index.js in models

--- models/index.js
+++ models/index.js
@@ -1,20 +1,67 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Product extends Model { }
+
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
+
+// Starter Code:
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
-
-// Products belongsTo Category
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
-
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+// const Product = require('./Product');
+// const Category = require('./Category');
+// const Tag = require('./Tag');
+// const ProductTag = require('./ProductTag');
+// // Products belongsTo Category
+// // Categories have many Products
+// // Products belongToMany Tags (through ProductTag)
+// // Tags belongToMany Products (through ProductTag)
+// module.exports = {
+//   Product,
+//   Category,
+//   Tag,
+//   ProductTag,
+// };
