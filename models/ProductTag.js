e5aabbb22f2e733b07fa37129cf54e2db341560a
lightoftwelve <info@lightoftwelve.com>
2023-08-26 21:48:08 -0400

    added tag.init to tag.js in models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,9 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
-
-class ProductTag extends Model {}
-
+class ProductTag extends Model { }
 ProductTag.init(
   {
     // define columns
@@ -16,5 +13,23 @@ ProductTag.init(
     modelName: 'product_tag',
   }
 );
-
 module.exports = ProductTag;
+
+
+
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
+// class ProductTag extends Model {}
+// ProductTag.init(
+//   {
+//     // define columns
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'product_tag',
+//   }
+// );
+// module.exports = ProductTag;
